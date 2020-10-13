9e57cdb4937105265564da79dc2b46501589f074
Eduardo Correia <eduardo.correia.ehsc@gmail.com>
2020-10-13 10:59:23 -0300

    Código em padrão MVC

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,40 +1,12 @@
 import express from 'express';
+import './database/connection';
 
-import { getRepository } from 'typeorm'
-import Orphanage from './models/Orphanage';
+import routes from './routes';
 
-import './database/connection'
 
 const app = express();
 
 app.use(express.json());
-
-app.post('/orphanages', async (req, res) => {
-    const {
-        name, 
-        latitude,
-        longitude,
-        about, 
-        instructions,
-        opening_hours,
-        open_on_weekends
-    } = req.body;
-
-    const orphanagesRepository = getRepository(Orphanage);
-
-    const orphanage = orphanagesRepository.create({
-        name, 
-        latitude,
-        longitude,
-        about, 
-        instructions,
-        opening_hours,
-        open_on_weekends
-    });
-
-    await orphanagesRepository.save(orphanage);
-
-    return res.status(201).json(orphanage);
-})
+app.use(routes);
 
 app.listen(3333);
