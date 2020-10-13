e0e97f90732760e2deef2e2683729dc4b1ed8a00
Eduardo Correia <eduardo.correia.ehsc@gmail.com>
2020-10-13 12:46:09 -0300

    Validação de dados

--- backend/src/errors/handler.ts
+++ backend/src/errors/handler.ts
@@ -1,7 +1,22 @@
-import { ErrorRequestHandler } from 'express'
+import { ErrorRequestHandler } from 'express';
+import { ValidationError } from 'yup';
+
+interface ValidationErrors{
+    [key: string]: string[];
+}
 
 const errorHandler: ErrorRequestHandler = (error, req, res, next) => {
     console.error(error)
+
+    if(error instanceof ValidationError){
+        let errors: ValidationErrors = {}
+
+        error.inner.forEach(err => {
+            errors[err.path] = err.errors;
+        });
+
+        return res.status(400).json({message: 'Internal server error', errors})
+    }
 
     return res.status(500).json({message: 'Internal server error'})
 }
