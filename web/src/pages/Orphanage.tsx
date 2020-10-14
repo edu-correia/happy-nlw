e09805d0bbc39f4f0bcb6e6a2017ed7696a13057
Eduardo Correia <eduardo.correia.ehsc@gmail.com>
2020-10-14 18:19:32 -0300

    "Componentizando" mais a aplicação

--- web/src/pages/Orphanage.tsx
+++ web/src/pages/Orphanage.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { FaWhatsapp } from "react-icons/fa";
-import { FiClock, FiInfo, FiArrowLeft } from "react-icons/fi";
+import { FiClock, FiInfo } from "react-icons/fi";
 import { Map, Marker, TileLayer } from "react-leaflet";
-import { useHistory } from 'react-router-dom';
 import L from 'leaflet';
 
 import mapMarkerImg from '../images/map-marker.svg';
 
 import '../styles/pages/orphanage.css';
+import SideBar from "../components/SideBar";
 
 const happyMapIcon = L.icon({
   iconUrl: mapMarkerImg,
@@ -18,19 +18,9 @@ const happyMapIcon = L.icon({
 })
 
 export default function Orphanage() {
-  const { goBack } = useHistory();
-
   return (
     <div id="page-orphanage">
-      <aside>
-        <img src={mapMarkerImg} alt="Happy" />
-
-        <footer>
-          <button type="button" onClick={goBack}>
-            <FiArrowLeft size={24} color="#FFF" />
-          </button>
-        </footer>
-      </aside>
+      <SideBar />
 
       <main>
         <div className="orphanage-details">
@@ -73,7 +63,7 @@ export default function Orphanage() {
                 doubleClickZoom={false}
               >
                 <TileLayer 
-                  url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+                  url={`https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
                 />
                 <Marker interactive={false} icon={happyMapIcon} position={[-27.2092052,-49.6401092]} />
               </Map>
--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,22 +1,12 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import {Map, TileLayer, Marker, Popup} from 'react-leaflet';
-import Leaflet from 'leaflet';
-
-import 'leaflet/dist/leaflet.css';
 
 import { FiPlus, FiArrowRight } from 'react-icons/fi';
 
 import '../styles/pages/orphanages-map.css'
-
 import mapMarkerImg from '../images/map-marker.svg';
-
-const mapIcon = Leaflet.icon({
-    iconUrl: mapMarkerImg,
-    iconSize: [58, 68],
-    iconAnchor: [29, 68],
-    popupAnchor: [170, 2]
-})
+import mapIcon from '../utils/mapIcon';
 
 function OrphanagesMap(){
     return(
@@ -25,7 +15,7 @@ function OrphanagesMap(){
                 <header>
                     <img src={mapMarkerImg} alt="happy"/>
                     <h2>Escolha um orfanato no mapa</h2>
-                    <p>Muitas criaças estão esperando sua visita :)</p>
+                    <p>Muitas crianças estão esperando sua visita!</p>
                 </header>
 
                 <footer>
