d063f627aba90b69d4eddadeb4e35a284e5a4d2f
Eduardo Correia <eduardo.correia.ehsc@gmail.com>
2020-10-14 10:44:26 -0300

    Alterações no estilo e adicionado páginas

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-import {Map, TileLayer} from 'react-leaflet';
+import {Map, TileLayer, Marker, Popup} from 'react-leaflet';
+import Leaflet from 'leaflet';
 
 import 'leaflet/dist/leaflet.css';
 
-import { FiPlus } from 'react-icons/fi';
+import { FiPlus, FiArrowRight } from 'react-icons/fi';
 
 import '../styles/pages/orphanages-map.css'
 
 import mapMarkerImg from '../images/map-marker.svg';
+
+const mapIcon = Leaflet.icon({
+    iconUrl: mapMarkerImg,
+    iconSize: [58, 68],
+    iconAnchor: [29, 68],
+    popupAnchor: [170, 2]
+})
 
 function OrphanagesMap(){
     return(
@@ -33,9 +41,24 @@ function OrphanagesMap(){
                 style={{width: '100%', height: '100%'}}
             >
                 <TileLayer url={`https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} />
+
+                <Marker 
+                    position={[-22.597897,-47.3964264]}
+                    icon={mapIcon}
+                >
+                    <Popup closeButton={false} minWidth={240} maxWidth={240} className="map-popup">
+                        Lar das meninas
+                        <Link to="/orphanages/1">
+                            <FiArrowRight 
+                                size={20}
+                                color="fff"
+                            />
+                        </Link>
+                    </Popup>
+                </Marker>
             </Map>
 
-            <Link to="" className="create-orphanage">
+            <Link to="/orphanages/create" className="create-orphanage">
                 <FiPlus 
                     size={32}
                     color="#fff"
--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -3,13 +3,18 @@ import {BrowserRouter, Switch, Route} from 'react-router-dom';
 
 import LandingPage from './pages/LandingPage';
 import OrphanagesMapPage from './pages/OrphanagesMap';
+import CreateOrphanagePage from './pages/CreateOrphanage';
+import OrphanagePage from './pages/Orphanage';
 
 function Routes(){
     return (
         <BrowserRouter>
             <Switch>
                 <Route path="/" exact component={LandingPage}/>
-                <Route path="/orphanages" component={OrphanagesMapPage}/>
+                <Route path="/app" component={OrphanagesMapPage}/>
+
+                <Route path="/orphanages/create" component={CreateOrphanagePage}/>
+                <Route path="/orphanages/:id" component={OrphanagePage}/>
             </Switch>
         </BrowserRouter>
     );
