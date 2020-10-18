bd7a051687b7684abe4b499d63ef418ab578c101
Eduardo Correia <eduardo.correia.ehsc@gmail.com>
2020-10-18 12:13:53 -0300

    Mostrando os pontos da API no mapa

--- mobile/src/pages/OrphanagesMap.tsx
+++ mobile/src/pages/OrphanagesMap.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { StyleSheet, Text, View, Dimensions } from 'react-native';
 import MapView, {Marker, Callout ,PROVIDER_GOOGLE} from 'react-native-maps';
 
@@ -7,9 +7,25 @@ import { useNavigation } from '@react-navigation/native';
 
 import mapMarker from '../images/map-marker.png';
 import { RectButton } from 'react-native-gesture-handler';
+import api from '../services/api';
+
+interface Orphanage{
+  id: number;
+  name: string;
+  latitude: number;
+  longitude: number;
+}
 
 export default function OrphanagesMap(){
+    const [orphanages, setOrphanages] = useState<Orphanage[]>([])
+
     const navigation = useNavigation();
+
+    useEffect(() => {
+      api.get('orphanages').then(res => {
+        setOrphanages(res.data);
+      })
+    }, [])
 
     function handleNavigateToOrphanageDetails(){
         navigation.navigate('OrphanageDetails');
@@ -22,49 +38,54 @@ export default function OrphanagesMap(){
 
     return (
         <View style={styles.container}>
-      <MapView 
-        provider={PROVIDER_GOOGLE}
-        style={styles.map} 
-       initialRegion={{
-         latitude: -22.5971366,
-         longitude: -47.3935564,
-         latitudeDelta: 0.008,
-         longitudeDelta: 0.008,
-       }}
-      >
-        <Marker 
-          icon={mapMarker}
-          calloutAnchor={{
-            x: 2.7,
-            y: 0.8
-          }}
-          coordinate={{
-            latitude: -22.5971366,
-            longitude: -47.3935564 
-          }}
-        >
-          <Callout tooltip onPress={handleNavigateToOrphanageDetails}>
-            <View style={styles.calloutContainer}>
-              <Text style={styles.calloutText}>Lar das meninas</Text>
-            </View>
-          </Callout>
-        </Marker>
-      </MapView>
+          <MapView 
+            provider={PROVIDER_GOOGLE}
+            style={styles.map} 
+            initialRegion={{
+              latitude: -22.5971366,
+              longitude: -47.3935564,
+              latitudeDelta: 0.008,
+              longitudeDelta: 0.008,
+            }}
+            >
+            {orphanages.map(orphanage => {
+              return (
+                <Marker 
+                  key={orphanage.id}
+                  icon={mapMarker}
+                  calloutAnchor={{
+                    x: 2.7,
+                    y: 0.8
+                  }}
+                  coordinate={{
+                    latitude: orphanage.latitude,
+                    longitude: orphanage.longitude 
+                  }}
+                >
+                  <Callout tooltip onPress={handleNavigateToOrphanageDetails}>
+                    <View style={styles.calloutContainer}>
+                      <Text style={styles.calloutText}>{orphanage.name}</Text>
+                    </View>
+                  </Callout>
+                </Marker>
+              )
+            })}
+          </MapView>
 
-      <View style={styles.footer}>
-          <Text style={styles.footerText}>
-            2 orfanatos encontrados
-          </Text>
+          <View style={styles.footer}>
+              <Text style={styles.footerText}>
+                2 orfanatos encontrados
+              </Text>
 
-          <RectButton style={styles.createOrphanageButton} onPress={handleNavigateToCreateOrphanage}>
-            <Feather 
-              name="plus"
-              size={20}
-              color="#fff"
-            />
-          </RectButton>
-      </View>
-    </View>
+              <RectButton style={styles.createOrphanageButton} onPress={handleNavigateToCreateOrphanage}>
+                <Feather 
+                  name="plus"
+                  size={20}
+                  color="#fff"
+                />
+              </RectButton>
+          </View>
+        </View> 
     )
 }
 
